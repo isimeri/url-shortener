22c09761501a3755981a870a5113b2f1e28890eb
isimeri <loge.lederun@gmail.com>
2022-04-14 02:47:25 +0300

    implemented this route and the other route

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const shortid = require('shortid');
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+//mongodb connect
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+    console.log("mongodb coneected")
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+const urlSchema = new mongoose.Schema({
+  longUrl: {type: String, required: true},
+  shortCode: {type: String}
+});
+const urlModel = mongoose.model("url", urlSchema);
+connectDB();
 
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,7 +35,37 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+app.post('/api/short', async (req, res) => {
+  const longUrl = req.body.url;
+  const shortCode = shortid.generate();
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+  try {
+    let urlDoc = await urlModel.findOne({longUrl});
+    if(urlDoc){
+      return res.json(urlDoc);
+    } else {
+      urlDoc = new urlModel({
+        longUrl,
+        shortCode
+      });
+
+      await urlDoc.save();
+      res.json({longUrl: urlDoc.longUrl, shortCode: urlDoc.shortCode});
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json(err);
+  }
+});
+app.get('/api/short/:shortUrl', async (req,res) => {
+  const url =  await urlModel.findOne({ shortCode: req.params.shortUrl });
+  if(!url){
+    return res.status(404).json({msg: "url not found"});
+  }
+  res.redirect(url.longUrl);
+
+});
+
+app.listen(process.env.PORT, function() {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
