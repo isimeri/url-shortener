09f496ba334ea4bcf4af619e995e1636de2538ed
isimeri <loge.lederun@gmail.com>
2022-04-16 14:49:56 +0300

    minor bug fix

--- server.js
+++ server.js
@@ -1,35 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const dns = require('dns');
-const shortid = require('shortid');
-const validUrl = require('valid-url');
 const app = express();
 
 const connectDB = require('./db/connection.js');
-const urlModel = require('./db/urlModel.js');
-const shortRouter = require('./router/shorten.js');
+const shortRouter = require('./router/short.js');
 
-//mongodb connect
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-//     console.log("mongodb connected")
-//   } catch (err) {
-//     console.error(err);
-//     process.exit(1);
-//   }
-// }
-// const urlSchema = new mongoose.Schema({
-//   longUrl: {type: String, required: true},
-//   shortCode: {type: String}
-// });
-// const urlModel = mongoose.model("url", urlSchema);
 connectDB();
 
 app.use(cors());
-
 app.use('/public', express.static(`${process.cwd()}/public`));
 app.use(express.urlencoded({extended: false}));
 app.use('/api/short', shortRouter);
@@ -42,7 +21,6 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
-
 
 app.listen(process.env.PORT, function() {
   console.log(`Listening on port ${process.env.PORT}`);
